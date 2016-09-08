bddc8b583070e95dec3405b310ecf6b647a06783
craig-stevenson <craig.stevenson@gmail.com>
2016-09-08 11:56:17 -0700

    Added Documentation

--- nmap-tools/utils.py
+++ nmap-tools/utils.py
@@ -1,11 +1,23 @@
 def get_ipv4_addresses(file):
+    """Parses the xml output of nmap and returns a list of the ipv4 addresses which are up
+
+    :param file: A string pointing to an xml output file from nmap
+    :return: A list of strings
+    """
     pass
 
 def get_ipv6_addresses(file):
+    """Parses the xml output of nmap and returns a list of the ipv6 addresses which are up
+
+    :param file:
+    :return:
+    """
     pass
 
 def get_services(file):
-    pass
+    """Parses the xml output of nmap and returns a list of the services which are running
 
-def get_services_by_ip():
+    :param file:
+    :return:
+    """
     pass
